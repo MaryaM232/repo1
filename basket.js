102f94ac981ab56c139fa1b49a546e2424cea978
MaryaM232 <mamonovamariya@mail.ru>
2022-09-25 09:45:24 +0300

    new code basket in catalog.html

--- basket.js
+++ basket.js
@@ -2,6 +2,7 @@
 
 const basketCounterEl = document.querySelector('.five');
 const basketTotalValueEl = document.querySelector('.basketTotalValue');
+const basketTotalEl = document.querySelector('.basketTotal');
 const basketEl = document.querySelector('.basket');
 
 document.querySelector('.header-basket').addEventListener('click', () => {
@@ -27,9 +28,45 @@ function addToCart(id, name, price) {
         basket[id] = { id, name, price, count: 0 };
     }
     basket[id].count++;
-    basketCounterEl.textContent = getTotalBaskeCount().toString();
+    basketCounterEl.textContent = getTotalBasketCount().toString();
+    basketTotalValueEl.textContent = getTotalBasketPrice().toFixed(2);
+    renderProductInBasket(id);
 }
 
-function getTotalBaskeCount() {
+function getTotalBasketCount() {
     return Object.values(basket).reduce((acc, product) => acc + product.count, 0);
+}
+
+function getTotalBasketPrice() {
+    return Object.values(basket)
+        .reduce((acc, product) => acc + product.count * product.price, 0);
+}
+
+function renderProductInBasket(id) {
+    const basketRowEl = basketEl
+        .querySelector(`.basketRow[data-productId="${id}"]`);
+    if (!basketRowEl) {
+        renderNewProductInBasket(id);
+        return
+    }
+
+    basketRowEl.querySelector('.productCount').textContent = basket[id].count;
+    basketRowEl.querySelector('.productTotalRow')
+        .textContent = basket[id].count * basket[id].price;
+}
+
+function renderNewProductInBasket(productId) {
+    const productRow = `
+        <div class="basketRow" data-productId="${productId}">
+            <div>${basket[productId].name}</div>
+            <div>
+                <span class="productCount">${basket[productId].count}</span> шт.
+            </div>
+            <div>$${basket[productId].price}</div>
+            <div>
+                $<span class="productTotalRow">${basket[productId].price * basket[productId].count}</span>
+            </div>
+        </div>
+    `;
+    basketTotalEl.insertAdjacentHTML('beforebegin', productRow);
 }
